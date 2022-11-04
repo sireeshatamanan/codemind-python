c6f89c26e0774178c0357f7e63e0f99929d74253
Tamanampudi Sireesha <81316259+sireeshatamanan@users.noreply.github.com>
2022-11-04 14:43:18 +0530

    n=int(input()) a=n while True:     c=0     for i in range(2,int(a**0.5)+1):         if a%i==0:             c=1             break     if c==0:         break     else:         a=a+1 b=n while True:     c=0     for i in range(2,int(b**0.5)+1):         if b%i==0:             c=1             break     if c==0:         break     else:         b=b-1 if(a-n>n-b):     print(n-b) else:     print(a-n)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,28 @@
+
+n=int(input())
+a=n
+while True:
+    c=0
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            c=1
+            break
+    if c==0:
+        break
+    else:
+        a=a+1
+b=n
+while True:
+    c=0
+    for i in range(2,int(b**0.5)+1):
+        if b%i==0:
+            c=1
+            break
+    if c==0:
+        break
+    else:
+        b=b-1
+if(a-n>n-b):
+    print(n-b)
+else:
+    print(a-n)
